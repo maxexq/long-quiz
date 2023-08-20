837dcbd17aab2f2f59dd05de34a343461d40234d
maxexq <parames.ajh@gmail.com>
2023-08-21 00:15:40 +0700

    feat: 🎸 add create quiz form, and add game model

--- src/components/quiz/QuizCreation.tsx
+++ src/components/quiz/QuizCreation.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import { z } from "zod";
 import {
@@ -12,6 +14,19 @@ import { zodResolver } from "@hookform/resolvers/zod";
 
 import { useForm } from "react-hook-form";
 import { quizCreationSchema } from "@/schemas/form/quiz";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { BookOpen, CopyCheck } from "lucide-react";
+import { Separator } from "../ui/separator";
 
 type Props = {};
 
@@ -26,6 +41,12 @@ const QuizCreation = (props: Props) => {
       type: "open_ended",
     },
   });
+
+  form.watch();
+
+  const onSubmit = (input: Input) => {
+    alert(JSON.stringify(input, null, 2));
+  };
   return (
     <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
       <Card>
@@ -33,7 +54,72 @@ const QuizCreation = (props: Props) => {
           <CardTitle className="text-2xl font-bold">Quiz Creation</CardTitle>
           <CardDescription>Choose a topic</CardDescription>
         </CardHeader>
-        <CardContent></CardContent>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="topic"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Topic</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter a topic..." {...field} />
+                    </FormControl>
+                    <FormDescription>Pleas provide a topic.</FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="amount"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Number of Questions</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="Enter a amount..."
+                        type="number"
+                        {...field}
+                        onChange={(e) => {
+                          form.setValue("amount", parseInt(e?.target?.value));
+                        }}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="flex justify-between">
+                <Button
+                  type="button"
+                  className="w-1/2 rounded-none rounded-l-lg"
+                  onClick={() => form.setValue("type", "mcq")}
+                  variant={
+                    form.getValues("type") === "mcq" ? "default" : "secondary"
+                  }
+                >
+                  <CopyCheck className="mr-2 h-4 w-4" /> Multiple choice
+                </Button>
+                <Separator orientation="vertical" />
+                <Button
+                  type="button"
+                  className="w-1/2 rounded-none rounded-r-lg"
+                  onClick={() => form.setValue("type", "open_ended")}
+                  variant={
+                    form.getValues("type") === "open_ended"
+                      ? "default"
+                      : "secondary"
+                  }
+                >
+                  <BookOpen className="mr-2 h-4 w-4" /> Open Ended
+                </Button>
+              </div>
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
+        </CardContent>
       </Card>
     </div>
   );
